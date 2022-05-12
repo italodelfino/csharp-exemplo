d1f99cb421eb7821c52199fd8c7d4c2515517cd3
italodelfino <italo_martins@outlook.com>
2022-05-12 14:40:53 -0300

    aulas csharp

---
+++ fluxoDeExecucao/aula1/Program.cs
@@ -0,0 +1,16 @@
+﻿int valor1;
+int valor2;
+int total;
+
+Console.WriteLine("digite o primeiro valor");
+valor1 = int.Parse(Console.ReadLine());
+Console.WriteLine("o primeiro valor é: " + valor1);
+
+Console.WriteLine("digite o segundo valor");
+valor2 = int.Parse(Console.ReadLine());
+Console.WriteLine("o segundo valor é: " + valor2);
+
+total = valor1 + valor2;
+Console.WriteLine("a soma dos dois valores é: " + total);
+
+
